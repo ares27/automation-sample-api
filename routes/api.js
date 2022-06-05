b0ce148a1a0525c3fed9456612f49e4807626271
Byron Jehoma <jehomasb@gmail.com>
2022-06-05 09:01:50 +0200

    updated

--- routes/api.js
+++ routes/api.js
@@ -1,27 +1,60 @@
 const express = require("express");
 const router = express.Router();
+const Joi = require("joi");
+const axios = require("axios");
 
 router
-  .get("/greet", (req, res) => {
-    if (!req.query.name || req.query.name === "")
-    res.status(400).send({
-        message: `error`,
-        description: `parameter 'name' is required and cannot be empty or blank.`,
-    });
-    res.send({
-        status: `Ok`,
-        greeting: `Hi ${req.query.name}`,
-    });
-  })
-  .get("/automation", (req, res) => {
-        const obj = {
-        status: 200,
-        message: 'connected',
-        description: {
-                host: 'T1 Automation',
-            }
-        }
+  .get("/", (req, res) => {
+    const obj = { description: "connected", host: "T1 Automation" };
     res.status(200).json(obj);
- });
+  })
+  .get("/jsonplaceholder", async (req, res) => {
+    try {
+      const response = await axios.get(
+        `https://jsonplaceholder.typicode.com/todos/`
+      );
+      const responseData = response.data;
+      res.send(responseData);
+    } catch (ex) {
+      res.status(404).send(ex.message);
+    }
+  })
+  .post("/data", async (req, res) => {
+    const schema = Joi.object({
+      title: Joi.string().required(),
+      fullName: Joi.string().min(3).required(),
+      departmentId: Joi.number().integer(),
+    });
+    const obj = {
+      title: req.body.title,
+      fullName: req.body.fullName,
+      departmentId: parseInt(req.body.departmentId),
+    };
+    const result = Joi.validate(obj, schema);
+    // console.log(result);
+    if (result.error) {
+      res.status(400).send(result.error.details[0].message);
+      return;
+    }
+
+    try {
+      const { title, fullName, departmentId } = obj;
+      res.status(201).send({
+        message: "You just posted!",
+        data: {
+          title,
+          fullName,
+          departmentId,
+        },
+      });
+    } catch (ex) {
+      res.send(ex.message);
+    }
+  });
+
+// WhatsApp
+router.post("/webhook", (req, res) => {
+  res.send(`Ok`);
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+const config = require("config");
 const PORT = process.env.PORT || 3099;
-const express = require('express');
+const express = require("express");
 const app = express();
 const api = require("./routes/api");
 app.use(express.json());
 app.use("/api", api);
+
 app.listen(PORT, () => {
-   console.log(`Server started on port: ${PORT}`);
+  console.log(`Server started on port: ${PORT}`);
 });
